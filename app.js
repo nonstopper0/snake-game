1d3b0ce33fe4389efb53d830086097cd4cdb6834
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2021-05-25 07:15:52 -0700

    onLoop function created

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 let container = document.querySelector('.game-container');
+let boardSize = 20
+let running = false;
+let outcome = "";
+let runTime = 0;
+let currentPos = [14, 14]
 
+function main() {
+    intialize()
+    setInterval(() => {
+        onLoop()
+    },  1000 / 30)
+}
 
 // fill document with divs
 function intialize() {
-    for (let i = 0; i < 32; i++) {
+    for (let i = 0; i < boardSize; i++) {
         let row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
-        for (let j = 0; j < 32; j++) {
+        for (let j = 0; j < boardSize; j++) {
             let column = document.createElement('div');
             column.classList.add('column');
             column.id = `${i} ${j}`
@@ -16,13 +27,18 @@ function intialize() {
     }
 }
 
-
-function main() {
-    intialize()
+function start() {
+    running = true;
 }
 
-function loop() {
 
+function onLoop() {
+    if (running) {
+        runTime += 1;
+        if (runTime % 5 === 0) {
+
+        }
+    } 
 }
 
 main();
