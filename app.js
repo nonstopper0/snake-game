bc63b02778c813e9e0266f4910cfed0fd934beb7
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2021-05-25 07:03:59 -0700

    using id system for divs, probably not the best method. will check again later

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 let container = document.querySelector('.game-container');
 
+
+// fill document with divs
 function intialize() {
     for (let i = 0; i < 32; i++) {
         let row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
-        for (let i = 0; i < 32; i++) {
+        for (let j = 0; j < 32; j++) {
             let column = document.createElement('div');
             column.classList.add('column');
-            column.style.width = '10px';
-            column.style.height = '10px';
-            column.style.backgroundColor = "black"
+            column.id = `${i} ${j}`
             row.appendChild(column)
         }
     }
